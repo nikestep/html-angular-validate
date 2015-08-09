3eb3ed2f43e95a1f664665ce9978dbf93b498dbd
Nik Estep <nik.estep@gmail.com>
2015-08-09 11:06:25 -0400

    Getting tests to run correctly and adding some reject cases

--- test/validateTest.js
+++ test/validateTest.js
@@ -1,20 +1,29 @@
 'use strict';
 
 var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
 var validate = require('../lib/validate.js');
 
-chai.use(chaiAsPromised);
-var expect = chai.expect();
 var should = chai.should();
+chai
+	.use(require('chai-properties'))
+	.use(require('chai-as-promised'));  // Must be last in the list!!
 
 describe("Validate", function() {
+	it("Reject no argument call", function() {
+		return validate.validate().should.be.rejected;
+	});
+
+	it("Reject empty files array", function() {
+		return validate.validate([]).should.be.rejected;
+	});
+
+	it("Reject files pattern that matches nothing", function() {
+		return validate.validate(["test/nowhere/**"]).should.eventually.be.rejected;
+	});
+
 	it("Validate Correct Full Documents", function() {
-		validate.validate(["test/html/correct/**"]).then(function(result) {
-			result.should.have.property("allpassed", true);
-			result.should.have.property("filessucceeded", 1);
-		}, function(err) {
-			expect(true).to.equal(false);  // Fail the test
+		return validate.validate(["test/html/correct/**"]).should.eventually.have.properties({
+			allpassed: true
 		});
 	});
 });
