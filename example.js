48db5680c7f184955d19449beab0d122f0361fe0
Nik Estep <nik.estep@gmail.com>
2015-08-09 12:20:00 -0400

    Fixing up example script

--- example.js
+++ example.js
@@ -1,7 +1,12 @@
 var pretty = require('js-object-pretty-print').pretty;
 var validate = require('./lib/validate.js').validate;
 
-validate(["test/sample/**", "test/sample_tmpls/**"]).then(function(data) {
+validate([
+	"test/html/valid/full/*",
+	"test/html/invalid/full/*"
+], {
+	customtags: ['custom-tag']
+}).then(function(data) {
 	console.log('Result:');
 	console.log(pretty(data));
 }, function(err) {
