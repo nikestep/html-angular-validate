8f92fc647748bc7bea0dfdecf24b007d65823cd4
Nik Estep <nik.estep@gmail.com>
2015-09-24 20:29:35 -0400

    Formatting example.js

--- example.js
+++ example.js
@@ -2,13 +2,13 @@ var pretty = require('js-object-pretty-print').pretty;
 var validate = require('./lib/validate.js').validate;
 
 validate([
-	"test/html/valid/full/*",
-	"test/html/invalid/full/*"
+  "test/html/valid/full/*",
+  "test/html/invalid/full/*"
 ], {
-	customtags: ['custom-tag']
+  customtags: ['custom-tag']
 }).then(function(data) {
-	console.log('Result:');
-	console.log(pretty(data));
+  console.log('Result:');
+  console.log(pretty(data));
 }, function(err) {
-	console.log('ERROR: ' + err);
+  console.log('ERROR: ' + err);
 });
